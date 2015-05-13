cdeacbe4dffb37b39e97d4414b9a46eb8b1930e6
aranda.loris <aranda.loris@gmail.com>
2015-05-13 08:41:45 -0400

    Bouton Ajouter fonctionnel / Enlever les lignes de test en HTML

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
 var main = function() {
-
-	$('.incident').click(function() {
+	
+		var selectLigne = function() {
+		$('.incident').click(function() {
 		$('.incident').removeClass("current");
 		$('.incident-description').hide();
 		$(this).addClass("current");
-		$(this).children('.incident-main').children('.incident-description').show();
+		$(this).children('.incident-main').children('.incident-description').show();						 
 	});
+	};
+	
+	selectLigne();
 
 	$('.btn-ajouter').click(function() {
-		$('.newIncident').toggle();
+		$('.newIncident').show();
 	});
 	
 	$('.btn-new').click(function() {
 		var dateFrom = $('.dateFrom').val();
-		var dateTo = $(this).children('.dateTo').val();
-		var incidentTitle = $(this).children('.incidentTitle').val();
-		var incidentDescription = $(this).children('incidentDescription').val();
+		var dateTo = $('.dateTo').val();
+		var incidentTitle = $('.incidentTitle').val();
+		var incidentDescription = $('.incidentDescription').val();
+		$('.incident').removeClass("current");
+		$('.incident-description').hide();
 		
-		$('<li>').text(dateFrom).appendTo('.incidents-list');
+		$('<li>').addClass("incident current").appendTo('.incidents-list');
+		$('<div>').addClass("incident-date").appendTo(".incident:last");
+		$(".incident-date:last").append("From : " + dateFrom + '<br>' + "To : " + dateTo);
+		$('<div>').addClass("incident-main").appendTo(".incident:last");
+		$('<div>').addClass("incident-title").appendTo(".incident-main:last");
+		$(".incident-title:last").text(incidentTitle);
+		$('<div>').addClass("incident-description").appendTo(".incident-main:last");
+		$(".incident-description:last").text(incidentDescription);
 		
+		$('.newIncident').hide();
+		selectLigne();
 	});
 
 };
